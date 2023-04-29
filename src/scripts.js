721a8c00b9ebc784c3147272dd8d4814c5a3c353
Michael Ward <gecotron@gmail.com>
2023-04-29 13:28:37 -0400

    Add weather widget

--- src/scripts.js
+++ src/scripts.js
@@ -9,3 +9,21 @@ let today = new Date();
 let time = today.toLocaleTimeString("en-US", options);
 
 document.getElementById("time").textContent = time;
+
+// display weather
+  const city = 'Ottawa';
+  const language = 'en';
+  const format = '%c+%C+(%f)';
+  const url = `https://wttr.in/${city}?format=${format}&lang=${language}`;
+
+  fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      const weatherInfo = data.trim();
+      const weatherElement = document.getElementById('weather');
+      weatherElement.textContent = weatherInfo;
+    })
+    .catch(error => {
+      const weatherElement = document.getElementById('weather');
+      weatherElement.textContent = "It's probably nice outside :)"
+    });
