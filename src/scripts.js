51316eec928b9bbb247ea05c54ca54b0c2418037
Michael Ward <mdwmage@proton.me>
2023-12-21 14:39:50 -0500

    links update

--- src/scripts.js
+++ src/scripts.js
@@ -11,19 +11,29 @@ let time = today.toLocaleTimeString("en-US", options);
 document.getElementById("time").textContent = time;
 
 // display weather
-  const city = 'Ottawa';
-  const language = 'en';
-  const format = '%c+%C+(%f)';
-  const url = `https://wttr.in/${city}?format=${format}&lang=${language}`;
+const city = "Ottawa";
+const language = "en";
+const format = "%c+%C+(%f)";
+const url = `https://wttr.in/${city}?format=${format}&lang=${language}`;
 
-  fetch(url)
-    .then(response => response.text())
-    .then(data => {
-      const weatherInfo = data.trim();
-      const weatherElement = document.getElementById('weather');
-      weatherElement.textContent = weatherInfo;
-    })
-    .catch(error => {
-      const weatherElement = document.getElementById('weather');
-      weatherElement.textContent = "It's probably nice outside :)"
-    });
+fetch(url)
+  .then((response) => response.text())
+  .then((data) => {
+    const weatherInfo = data.trim();
+    const weatherElement = document.getElementById("weather");
+    weatherElement.textContent = weatherInfo;
+  })
+  .catch((error) => {
+    const weatherElement = document.getElementById("weather");
+    weatherElement.textContent = "It's probably nice outside :)";
+  });
+
+function externalLinks() {
+  for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+    var b = c[a];
+    b.getAttribute("href") &&
+      b.hostname !== location.hostname &&
+      (b.target = "_blank");
+  }
+}
+externalLinks();
